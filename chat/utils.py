bbbc9f43379609a141606e15db32dfa5f84ae421
bhpark <88michael@naver.com>
2025-04-16 14:15:56 +0900

    Capitalize input text if it starts with an alphabetic character and is ASCII

--- chat/utils.py
+++ chat/utils.py
@@ -6,6 +6,9 @@ def translate_text(text, target_lang):
     auth_key = settings.DEEPL_API_KEY
     translator = deepl.Translator(auth_key)
 
+    if text and text[0].isalpha() and text.isascii():
+        text = text.capitalize()
+
     result = translator.translate_text(text, target_lang=target_lang)
 
     return result.text
